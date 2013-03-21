d43bec2f69238122cd7f6cec156850afc0ef036f
Matthew Johnson <mattjj@csail.mit.edu>
2013-03-20 23:22:17 -0400

    removed temporary test binding

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -2,5 +2,4 @@ function fish_user_key_bindings
     bind \e\[1\;9A 'history-token-search-backward'
     bind \e\[1\;9C 'forward-word'
     bind \e\[1\;9D 'backward-word'
-    bind \cf 'echo hi'
 end
