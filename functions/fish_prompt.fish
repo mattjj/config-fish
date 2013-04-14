67be9d866fa84b7c3d2005e01e70a3b7eef7c82e
Matthew Johnson <mattjj@csail.mit.edu>
2013-04-14 13:07:27 -0400

    printf -> echo

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,23 +1,23 @@
 function fish_prompt
    set_color blue
-   printf '%s' (whoami)
+   echo -n (whoami)
    set_color normal
 
-   printf ' at '
+   echo -n ' at '
    set_color yellow
-   printf '%s' (hostname|cut -d . -f 1)
+   echo -n (hostname|cut -d . -f 1)
    set_color normal
 
-   printf ' in '
+   echo -n ' in '
    set_color $fish_color_cwd
-   printf '%s' (echo $PWD | sed -e "s|^$HOME|~|")
+   echo -n (pwd | sed -e "s|^$HOME|~|")
    set_color normal
 
    __fish_git_prompt " on %s"
 
    echo
 
-   printf '○ '
+   echo -n '○ '
    set_color normal
 
    autojump -a $PWD > /dev/null &
